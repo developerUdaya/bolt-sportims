527b52e096791f68e00c7012be6e7e465d2f3805
developerUdaya <133474702+developerUdaya@users.noreply.github.com>
2025-06-19 21:58:34 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard';
 import Events from './pages/Events';
-import EventParticipation from './pages/eventDetails/EventParticipation';
-import PaymentReport from './pages/eventDetails/PaymentReport';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import EventParticipantsDetails from './pages/eventDetails/EventParticipantsDetails';
 
 function App() {
   return (
@@ -26,9 +23,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="events" element={<Events />} />
-          <Route path="/eventsDetails/participation" element={<EventParticipation />} />
-          <Route path="/eventsDetails/event-participation/:eventId" element={<EventParticipantsDetails />} />
-          <Route path="/eventsDetails/payment" element={<PaymentReport />} />
         </Route>
 
       
